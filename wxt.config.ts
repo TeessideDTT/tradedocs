adb8e484d3a445265a3cd2cd93a66f86adaf2a3e
Solomon Nsumei <solnsumei@gmail.com>
2026-07-10 12:58:09 +0100

    bump version

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   }),
   manifest: {
-    version: '0.1.0',
+    version: '0.1.1',
     permissions: ['sidePanel', 'storage'],
     name: 'TDO Builder',
     description: 'Create UN/CEFACT compliant trade docs directly in your browser.',
