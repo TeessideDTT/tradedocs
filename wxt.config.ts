e042f81c9ca644dd979af3ab24067b48ccdad784
Solomon Nsumei <solnsumei@gmail.com>
2026-07-10 12:27:55 +0100

    prepare versioning for chrome store

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   }),
   manifest: {
-    version: '0.0.2',
+    version: '0.1.0',
     permissions: ['sidePanel', 'storage'],
     name: 'TDO Builder',
     description: 'Create UN/CEFACT compliant trade docs directly in your browser.',
